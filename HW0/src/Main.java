92fc3f440589aa0f33c69ff9712245e7f4dc933c
vsawce <vsaw30@gmail.com>
2023-08-31 17:02:59 -0700

    Added simple introductory program about myself

--- HW0/src/Main.java
+++ HW0/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        Person vincent = new Person("Vincent", 21, "Golf and Racing");
+        vincent.introduce_self();
     }
+}
+
+class Person {
+    String name;
+    int age;
+    String hobbies;
+
+    Person() {
+        name = "N/A";
+        age = -1;
+        hobbies = "N/A";
+    }
+
+    Person(String n, int a, String h) {
+        name = n;
+        age = a;
+        hobbies = h;
+    }
+
+    void introduce_self() {
+        System.out.printf("Hello, my name is %s.\nI am %d years old.\nFor hobbies, I like %s!\n", name, age, hobbies);
+    }
+
 }
