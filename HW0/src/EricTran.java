b86ae2d80211688915b23c63a6b5a20d898151a3
eric <131493733+Trannics@users.noreply.github.com>
2023-09-04 17:31:20 -0700

    Update EricTran.java

--- HW0/src/EricTran.java
+++ HW0/src/EricTran.java
@@ -1,25 +1,31 @@
 public class EricTran {
+    public static void main(String[] args) {
+        TeddyBear myTeddyBear = new TeddyBear("Blue", 2);
+        System.out.println("Someone: So tell me about your teddy bear.");
+        System.out.println("You:");
+        myTeddyBear.printName();
+        myTeddyBear.printAge();
+    }
+}
+
+class TeddyBear {
     // Declare two instance variables
     private String name;
     private int age;
 
     // Teddy Bear constructor
-    public EricTran(String theName, int theAge) {
+    public TeddyBear(String theName, int theAge) {
         name = theName;
         age = theAge;
     }
+
     // Prints the name of the Teddy Bear
     public void printName() {
-        System.out.println("The name of your Teddy Bear is: " + name);
-    }
-    // Prints the age of the Teddy Bear
-    public void printAge() {
-        System.out.println("The age of your Teddy Bear is: " + age + " years old");
+        System.out.println("The name of my Teddy Bear is: " + name);
     }
 
-    public static void main(String[] args) {
-        EricTran myTeddyBear = new EricTran("Blue", 5);
-        myTeddyBear.printName();
-        myTeddyBear.printAge();
+    // Prints the age of the Teddy Bear
+    public void printAge() {
+        System.out.println("The age of my Teddy Bear is: " + age + " years old");
     }
 }
