8e3fac4097d11a352686a196846df295ecef6772
Jaydev Patel <144302349+Jahobie@users.noreply.github.com>
2023-11-18 23:38:25 -0800

    Delete homework/src/HW1/submissions/partA/airbnb directory

--- homework/src/HW1/submissions/partA/airbnb/DiscountedHotel.java
+++
@@ -1,7 +0,0 @@
-package jaydevpatel.airbnb;
-
-public class DiscountedHotel extends Hotel {
-    int calculateTotalPrice(int numberOfNights) {
-        return super.calculateTotalPrice(numberOfNights) - 50;
-    }
-}
