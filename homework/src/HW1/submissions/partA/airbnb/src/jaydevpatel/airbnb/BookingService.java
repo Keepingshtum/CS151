5611bb813aeddd2552f9c38539d018c732c613ec
Jaydev Patel <144302349+Jahobie@users.noreply.github.com>
2023-11-18 23:48:38 -0800

    Add files via upload

---
+++ homework/src/HW1/submissions/partA/airbnb/src/jaydevpatel/airbnb/BookingService.java
@@ -0,0 +1,10 @@
+package jaydevpatel.airbnb;
+
+public interface BookingService {
+    int calculateTotalPrice(Hotel hotel, int numberOfNights);
+
+    double calculateTotalPriceWithTax(TaxableHotel hotel, int numberOfNights);
+}
+
+
+
