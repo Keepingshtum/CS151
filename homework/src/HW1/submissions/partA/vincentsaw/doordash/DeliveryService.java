543d9c2508f34bf3a32f06385cacbea4c06d79c0
vsawce <vsaw30@gmail.com>
2023-11-20 16:06:20 -0800

    Completed partA doordash

--- homework/src/HW1/submissions/partA/vincentsaw/doordash/DeliveryService.java
+++ homework/src/HW1/submissions/partA/vincentsaw/doordash/DeliveryService.java
@@ -2,5 +2,4 @@ package HW1.submissions.partA.vincentsaw.doordash;
 
 public interface DeliveryService {
     void deliverFood(Restaurant restaurant, Customer customer);
-    void trackPackage(String trackingNumber);
 }
