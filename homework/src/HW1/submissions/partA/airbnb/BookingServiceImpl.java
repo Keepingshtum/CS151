6792169884d437d851db7edbff5383c1d657d0f1
Trilinear <simontr123@gmail.com>
2023-11-21 19:57:13 -0800

    Removed BookingServiceImpl Forgot that this file is now defunct in my submission since I am using

--- homework/src/HW1/submissions/partA/airbnb/BookingServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package HW1.submissions.partA.airbnb;
-
-public class BookingServiceImpl implements BookingService {
-    @Override
-    public int calculateTotalPrice(int numberOfNights) {
-        int totalPrice = calculateTotalPrice(numberOfNights);
-        return totalPrice;
-    }
-}
