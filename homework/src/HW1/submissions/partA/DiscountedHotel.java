3a886c3447323dd5f07ef85fb0c103f815c85cf6
Achur260 <145436522+Achur260@users.noreply.github.com>
2023-11-24 00:06:03 -0800

    Delete homework/src/HW1/submissions/partA/DiscountedHotel.java

--- homework/src/HW1/submissions/partA/DiscountedHotel.java
+++
@@ -1,9 +0,0 @@
-package airbnb;
-
-public class DiscountedHotel implements Hotel {
-	
-	@Override
-    public int calculateTotalPrice(int numberOfNights) {
-        return 100*numberOfNights - 50;
-    }
-}
