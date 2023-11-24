52857c941ddc97f2d16e1d092c237ef1689026c1
Achur260 <145436522+Achur260@users.noreply.github.com>
2023-11-24 00:04:52 -0800

    Delete homework/src/HW1/submissions/partA/BookingServiceImpl.java

--- homework/src/HW1/submissions/partA/BookingServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package airbnb;
-
-public class BookingServiceImpl implements BookingService {
-    @Override
-    public int calculateTotalPrice(Hotel hotel, int numberOfNights) {
-        int totalPrice = hotel.calculateTotalPrice(numberOfNights);
-        return totalPrice;
-    }
-}
