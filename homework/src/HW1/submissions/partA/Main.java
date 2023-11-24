be702e3a7fabed33a9cb2f51960f14872f173e15
Achur260 <145436522+Achur260@users.noreply.github.com>
2023-11-24 00:06:33 -0800

    Delete homework/src/HW1/submissions/partA/Main.java

--- homework/src/HW1/submissions/partA/Main.java
+++
@@ -1,9 +0,0 @@
-package paypal;
-
-public class Main {
-    public static void main(String[] args) {
-        PayPalGateway paymentProcessor = new PaymentProcessor();
-        Account account = new Account("1");
-        paymentProcessor.processPayment(account,100.0, 101);
-    }
-}
