e0667aeebe51b854591fccc17813313d6be38ef0
Achur260 <145436522+Achur260@users.noreply.github.com>
2023-11-24 00:06:40 -0800

    Delete homework/src/HW1/submissions/partA/PayPalGateway.java

--- homework/src/HW1/submissions/partA/PayPalGateway.java
+++
@@ -1,6 +0,0 @@
-package paypal;
-
-public interface PayPalGateway {
-    void processPayment(Account account, double amount);
-    void processPayment(Account account, double amount, double balance);
-}
